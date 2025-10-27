412f0e8c665bc57655b9dec47c86d060fec2c0ac
Davi <baptistadossantosdavi@gmail.com>
2025-10-27 18:26:44 -0300

    Finalizando

--- src/modules/products/product.repo.memory.js
+++ src/modules/products/product.repo.memory.js
@@ -25,4 +25,22 @@ export const makeUserRepoMemory = () => {
             return { items: paged, page, limit, total}
          
     }
+    const findById = async({id}) => items.find(i => i.id === id) ?? null 
+
+    const update = async ({ id, data }) => {
+        const idx = items.findIndex( i => i.id === id)
+
+        if (idx < 0) return null
+
+        items[idx] = { ...items[idx], ...data }
+
+        return items[idx]
+    }
+    const remove = async ({ id}) => {
+        const before = items.length
+
+        items = items.filter(i => i.id === id)
+        return items.length < before
+     }
+     return { create, findAll, findById, update, remove }
 }
